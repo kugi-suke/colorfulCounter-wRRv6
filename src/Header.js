238df4c4dcb9a78e3356b49d8e487a62b6e6a3c6
kugi-suke <kosuke.k.h91009@docomo.ne.jp>
2022-01-12 13:56:26 +0900

    import css

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './Header.css';
 
 function Header() {
     return (
-        <div className="Header">
-            <p>React Router Test: Colorful Counter</p>
+        <div>
+            <div className="Header">
+                <p>React Router Test: Colorful Counter</p>
+            </div>
+            <nav className="Nav">
+                <Link to="/">red counter</Link> |{" "}
+                <Link to="/blue">blue counter</Link> |{" "}
+                <Link to="/yellow">yellow counter</Link>
+            </nav>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 import App from "./App";
 import App2 from "./routes/App2";
 import App3 from "./routes/App3";
