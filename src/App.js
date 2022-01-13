631ecde46c07e8add277ec126b443762e5a7c1c2
kugi-suke <kosuke.k.h91009@docomo.ne.jp>
2022-01-13 16:44:19 +0900

    introduce axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 type Props = {
   initialCount: number,
+  initialText: string,
 };
 
-function App({ initialCount }: Props) {
+function App({ initialCount, initialText }: Props) {
   const [count, setCount] = useState(initialCount);
+  const [definedCount, setDefCount] = useState(initialCount);
+  const [text, setText] = useState(initialText);
+
+  useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/todos/' + count)
+        .then(res => {
+            setText(res.data.title)
+        })
+        .catch(() => {
+            setText(initialText)
+            console.log('ERROR! jsonplaceholder has 1~200 data...')
+        })
+    });
 
   return (
     <div className="App" data-test="component-app">
@@ -18,6 +33,14 @@ function App({ initialCount }: Props) {
           <button onClick={() => setCount(count - 1)}>-1</button>
           <button onClick={() => setCount(initialCount)}>clear</button>
         </div>
+        <div className="Submit-form">
+          <input type="text" value={definedCount} onChange={d => setDefCount(d.target.value)}></input>
+          <button className="submitButton" type="submit" onClick={() => setCount(+definedCount)}>Set Count</button>
+        </div>
+      </div>
+      <div className="Todos">
+          <p>from jsonplaceholeder/todos</p>
+          <p>{text}</p>
       </div>
     </div>
   );
@@ -26,6 +49,7 @@ function App({ initialCount }: Props) {
 
 App.defaultProps = {
   initialCount: 0,
+  initialText: "",
 };
 
 export default App;
--- src/routes/App3.js
+++ src/routes/App3.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../App.css';
 
 type Props = {
   initialCount: number,
+  initialText: string,
 };
 
-function App3({ initialCount }: Props) {
+function App({ initialCount, initialText }: Props) {
   const [count, setCount] = useState(initialCount);
+  const [definedCount, setDefCount] = useState(initialCount);
+  const [text, setText] = useState(initialText);
+
+  useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/todos/' + count)
+        .then(res => {
+            setText(res.data.title)
+        })
+        .catch(() => {
+            setText(initialText)
+            console.log('ERROR! jsonplaceholder has 1~200 data...')
+        })
+    });
 
   return (
-    <div className="App3" data-test="component-app3">
-      <div className="App3-counter">
+    <div className="App" data-test="component-app">
+      <div className="App-counter">
         <p>Yellow Counter</p>
         <p>Count: {count}</p>
         <div>
@@ -18,14 +33,23 @@ function App3({ initialCount }: Props) {
           <button onClick={() => setCount(count - 1)}>-1</button>
           <button onClick={() => setCount(initialCount)}>clear</button>
         </div>
+        <div className="Submit-form">
+          <input type="text" value={definedCount} onChange={d => setDefCount(d.target.value)}></input>
+          <button className="submitButton" type="submit" onClick={() => setCount(+definedCount)}>Set Count</button>
+        </div>
+      </div>
+      <div className="Todos">
+          <p>from jsonplaceholeder/todos</p>
+          <p>{text}</p>
       </div>
     </div>
   );
 };
 // https://stackoverflow.com/questions/37902849/import-and-export-may-only-appear-at-the-top-level
 
-App3.defaultProps = {
+App.defaultProps = {
   initialCount: 0,
+  initialText: "",
 };
 
-export default App3;
+export default App;
